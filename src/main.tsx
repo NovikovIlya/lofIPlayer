f1a62f901da425c0fd99072e3b68562f8c99ec60
novikov.i.sergeevich <novikov.i.sergeevich@gmail.com>
2025-07-25 11:46:53 +0000

    1

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import bridge from '@vkontakte/vk-bridge';
+
+bridge.send('VKWebAppInit')
+  .then((data) => { 
+    if (data.result) {
+      // Приложение инициализировано
+    } else {
+      // Ошибка
+    }
+  })
+  .catch((error) => {
+    // Ошибка
+    console.log(error);
+  });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
